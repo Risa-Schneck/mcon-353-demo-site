2869e0da8c5d09c4fc449b1c179f8a1dd11cb82e
Risa-Schneck <92038337+Risa-Schneck@users.noreply.github.com>
2022-02-24 19:24:57 -0500

    adding files from error

--- components/app/App.js
+++ components/app/App.js
@@ -1,9 +1,18 @@
 import {Todo} from "../todo/todo";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Header} from "../header/header";
 
 function App() {
   return(
     <div className = "App">
-        <Todo/>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/todo" elemtn={<Todo />} />
+        </Routes>
+      </BrowserRouter>
+      <Todo/>
     </div>
   )
   
